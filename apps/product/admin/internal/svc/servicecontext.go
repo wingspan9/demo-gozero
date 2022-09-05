a7897535fe0d4a31194a49afda978753a4b82b6d
wingspan <wingspan9@foxmail.com>
2022-09-05 21:34:49 +0800

    init directory

---
+++ apps/product/admin/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"demo-gozero/apps/product/admin/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
